c9f80e3f831885fa187ded6a120ebbb2d0add122
ZJia-goo <zengjiagoo@gmail.com>
2025-04-08 16:20:57 +0800

    Add files via upload

---
+++ train_scripts/vit/vtab/caltech101/train_wst.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=0 python train_vit_vtab.py \
+  --data_dir  /path/to/vtab-1k \
+  --load_path /path/to/Vit-B_16.npz \
+  --dataset caltech101 \
+  --model vit_base_patch16_224_in21k \
+  --batch_size 32 \
+  --batch_size_test 256 \
+  --epochs 100 \
+  --warmup_epochs 10 \
+  --r 2 \
+  --scale 10 \
+  --weight_decay 0.05 \
+  --lr 0.001 \
+  --drop_path 0.0 \
+  --amp \
+  --prefetcher
